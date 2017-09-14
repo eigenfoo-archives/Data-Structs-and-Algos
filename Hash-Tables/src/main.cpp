7b86b5c1831a065f2cd3ecafe5c659e8e5012970
George Ho <georgh0021@gmail.com>
2017-09-14 14:35:52 -0400

    BLD skeleton for spell checker

---
+++ Hash-Tables/src/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctime>
+#include "hash.hpp"
+
+std::ifstream infile;
+std::ofstream outfile;
+
+void openInputStream(std::ifstream &in, bool openDict = true);
+void openOutputStream(std::ofstream &out);
+void loadDictionary();
+void checkDocument();
+
+int main() {
+    clock_t t1, t2;
+    double timeDiff;
+
+    t1 = clock();
+    loadDictionary();
+    t2 = clock();
+    timeDiff = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+    std::cout << "Total time (in seconds) to load dictionary: " << timeDiff << std::endl;
+
+    t1 = clock();
+    checkDocument();
+    t2 = clock();
+    timeDiff = ((double) (t2 - t1)) / CLOCKS_PER_SEC;
+    std::cout << "Total time (in seconds) to check document: " << timeDiff << std::endl;
+}
+
+//  Prompts the user for the dictionary or input file and opens the
+//  file for reading
+void openInputStream(std::ifstream &in, bool openDict) {
+    std::string inname;
+    if (openDict) {
+        std::cout << "Enter name of dictionary: ";
+    }
+    else {
+        std::cout << "Enter name of output file: ";
+    }
+    std::cin >> inname;
+    in.open(inname.c_str());
+}
+
+//  Prompts the user for an output file and opens the file for writing
+void openOutputStream(std::ofstream &out) {
+    std::string outname;
+    std::cout << "Enter name of output file: ";
+    std::cin >> outname;
+    out.open(outname.c_str());
+}
+
+void loadDictionary() {
+
+}
+
+void checkDocument() {
+
+}
