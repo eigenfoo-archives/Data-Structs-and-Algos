9059d8f3e9db697a6fb503098b27778b29e0f743
George Ho <georgh0021@gmail.com>
2017-09-13 22:10:12 -0400

    BUG forgot to include HashTable:: to all funcs

--- Hash-Tables/src/hash.cpp
+++ Hash-Tables/src/hash.cpp
@@ -14,23 +14,23 @@ HashTable::HashTable(int size) {
     this->data.resize(tableSize, empty);
 }
 
-int insert(const std::string &key, void *pv) {
+int HashTable::insert(const std::string &key, void *pv) {
     return 0;
 }
 
-bool contains(const std::string &key) {
+bool HashTable::contains(const std::string &key) {
     return true;
 }
 
-void *getPointer(const std::string &key, bool *b) {
+void * HashTable::getPointer(const std::string &key, bool *b) {
     return nullptr;
 }
 
-int setPointer(const std::string &key, void *pv) {
+int HashTable::setPointer(const std::string &key, void *pv) {
     return 0;
 }
 
-static unsigned int hash(const std::string &key) {
+unsigned int HashTable::hash(const std::string &key) {
     // Fowler-Noll-Vo hash function
     // https://www.programmingalgorithms.com/algorithm/fnv-hash?lang=C%2B%2B
 	const unsigned int fnv_prime = 0x811C9DC5;
@@ -46,11 +46,11 @@ static unsigned int hash(const std::string &key) {
 	return hash;
 }
 
-int findPos(const std::string &key) {
+int HashTable::findPos(const std::string &key) {
     return 0;
 }
 
-bool rehash() {
+bool HashTable::rehash() {
     return true;
 }
 
