40e63a65abbc5a1abe2b656392488794b36004c2
George Ho <georgh0021@gmail.com>
2017-09-16 12:49:16 -0400

    BUG fixed typo

--- Hash-Tables/test/make_wordlist.py
+++ Hash-Tables/test/make_wordlist.py
@@ -2,7 +2,7 @@ import string
 
 translator = str.maketrans('', '', string.punctuation + '“”')
 
-with open('wp.txt', 'r') as f:
+with open('warpeace.txt', 'r') as f:
     for line in f:
         for word in line.split():
             print(word.translate(translator))
