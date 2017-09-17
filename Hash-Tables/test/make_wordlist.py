7285cd012ba87e2ee452d162f46fc5d5d9506767
George Ho <georgh0021@gmail.com>
2017-09-17 11:57:58 -0400

    TST made more tests

--- Hash-Tables/test/make_wordlist.py
+++ Hash-Tables/test/make_wordlist.py
@@ -1,8 +1,19 @@
+import os
+import subprocess
 import string
 
-translator = str.maketrans('', '', string.punctuation + '“”')
+remove = string.punctuation.replace('-', '')
+translator = str.maketrans('', '', remove + '“”‘’')
 
-with open('warpeace.txt', 'r') as f:
-    for line in f:
-        for word in line.split():
-            print(word.translate(translator))
+txt_files = filter(lambda x: x[-4:] == '.txt', os.listdir('./'))
+
+subprocess.run(['touch', 'wordlist_george'])
+
+with open('wordlist_george', 'w') as outfile:
+    for txt in txt_files:
+        with open(txt, 'r') as infile:
+            for line in infile:
+                for word in line.split():
+                    outfile.write(word.translate(translator) + '\n')
+
+subprocess.run(['sort', '-u', '-o', 'wordlist_george', 'wordlist_george'])
