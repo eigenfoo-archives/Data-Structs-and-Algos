9926d17d470588bed2f71ca4136af320fdda4fc8
George Ho <georgh0021@gmail.com>
2017-09-27 16:38:32 -0400

    BLD last adjustments to tests for hash

---
+++ Hash-Tables/test/make_wordlist.py
@@ -0,0 +1,26 @@
+'''
+Python3 script to read in all .txt files in current directory, and create
+a new file in Sable's dictionary format.
+This script was developed for English .txt files from Project Gutenberg in
+mind: https://www.gutenberg.org/
+'''
+
+import os
+import subprocess
+import string
+
+remove = string.punctuation.replace("-'", '')
+translator = str.maketrans('', '', remove + '“”‘’')
+
+txt_files = filter(lambda x: x[-4:] == '.txt', os.listdir('./'))
+
+subprocess.run(['touch', 'wordlist_george'])
+
+with open('wordlist_george', 'w') as outfile:
+    for txt in txt_files:
+        with open(txt, 'r') as infile:
+            for line in infile:
+                for word in line.split():
+                    outfile.write(word.translate(translator) + '\n')
+
+subprocess.run(['sort', '-u', '-o', 'wordlist_george', 'wordlist_george'])
