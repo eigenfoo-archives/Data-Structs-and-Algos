73f29c9e5e9d8ea8fd9c338a152c34762b8e2ab6
George Ho <georgh0021@gmail.com>
2017-11-08 22:02:05 -0500

    TST made tests smaller

--- Dijkstra/test/make_graph.py
+++ Dijkstra/test/make_graph.py
@@ -1,7 +1,7 @@
 import random
 
 
-num_vertices = 10000
+num_vertices = 1000
 with open('biggraph.txt', 'w') as outfile:
     for vertex in range(num_vertices):
         num_neighbors = random.randint(0, num_vertices/10)
